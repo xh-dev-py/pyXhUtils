412188fc2c78c89afb8d2a8eaad393999f40529a
xeth hung <adamhung12@gmail.com>
2023-01-09 06:47:52 +0800

    fix missing component

--- src/file_changes_xh/__init__.py
+++ src/file_changes_xh/__init__.py
@@ -1,1 +1,1 @@
-from .FileChanges import FileProgress, DeltaRead, DeltaType, DateFormattingRenameHandler, RenameHandler, ChangeHandler
+from .FileChanges import FileProgressUtils, FileProgress, DeltaRead, DeltaType, DateFormattingRenameHandler, RenameHandler, ChangeHandler
