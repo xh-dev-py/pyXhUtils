be4cfe3d569eb1af0f3f6a65033d4ad45286c7ca
xeth hung <adamhung12@gmail.com>
2023-04-27 12:18:33 +0800

    add check_not_exists param

--- src/xh_file_utils/FileUtils.py
+++ src/xh_file_utils/FileUtils.py
@@ -17,9 +17,11 @@ class FileUtils:
             return f.read()
 
     @staticmethod
-    def check_file(path, label="File", check_exists=True, check_is_file=False, check_is_directory=False):
+    def check_file(path, label="File", check_exists=False, check_is_file=False, check_is_directory=False, check_not_exists=False):
         if check_exists and not os.path.exists(path):
             raise Exception(f"File[{path}] not exists")
+        if check_not_exists and os.path.exists(path):
+            raise Exception(f"File[{path}] already exists")
         if check_is_file and not os.path.isfile(path):
             raise Exception(f"{label}[{path}] is not file")
         if check_is_directory and not os.path.isdir(path):
