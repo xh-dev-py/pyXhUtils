b014c27459b54917084dfff9f27775c6d88dd03b
xeth hung <xethhung@xh-network.xyz>
2024-12-03 11:37:52 +0800

    fix timezone issue

--- src/xh_time_utils/utils.py
+++ src/xh_time_utils/utils.py
@@ -4,8 +4,8 @@ import datetime as dt
 time_format= '%Y-%m-%dT%H:%M:%S%z'
 hktz = zoneinfo.ZoneInfo("Asia/Hong_Kong")
 jptz = zoneinfo.ZoneInfo("Asia/Tokyo")
-def to_time_str(d, tz: hktz, format=time_format):
-    return d.astimezone(tz).strftime(format)
+def to_time_str(d, tz=hktz, format=time_format):
+    return dt.datetime(d.year,d.month,d.day,d.hour, d.minute, d.second, d.microsecond, tzinfo=tz).strftime(format)
 
 def from_str_to_time(date_str, tz=hktz, format=time_format)->dt.datetime|None:
     return dt.datetime.strptime(date_str.replace(" +","+"), format).astimezone(tz) if date_str is not None else None
